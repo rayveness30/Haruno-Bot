9af1c6c7d66ebfbeffbc822aedf1d5f1cd9381f3
rayveness30 <106240087+rayveness30@users.noreply.github.com>
2022-05-26 21:43:44 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-console.log('Please Wait...!\nStarting Ray Bot...')
-
-import { join, dirname } from 'path'
-import { createRequire } from "module";
-import { fileURLToPath } from 'url'
-import { setupMaster, fork } from 'cluster'
-import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts';
-import { createInterface } from 'readline'
-import yargs from 'yargs'
-
-// https://stackoverflow.com/a/50052194
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
-const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
-const { say } = cfonts
-const rl = createInterface(process.stdin, process.stdout)
-
-say('Ray \nWhatsApp Bot\nMulti-device beta', {
-  font: 'chrome',
+console.log('Memulai...')
+let { spawn } = require('child_process')
+let path = require('path')
+let fs = require('fs')
+let package = require('./package.json')
+const CFonts = require('cfonts')
+CFonts.say('Haruno Bot', {
+  colors: ['#f2aa4c'],
+  font: 'block',
   align: 'center',
-  gradient: ['red', 'magenta']
 })
-say(`by ${author.name}`, {
+CFonts.say(`'${package.name}' Oleh @${package.author.name || package.author}`, {
+  colors: ['#f2aa4c'],
   font: 'console',
   align: 'center',
-  gradient: ['red', 'magenta']
 })
 
 var isRunning = false
@@ -35,22 +23,20 @@ var isRunning = false
 function start(file) {
   if (isRunning) return
   isRunning = true
-  let args = [join(__dirname, file), ...process.argv.slice(2)]
-  say([process.argv[0], ...args].join(' '), {
+  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
+  CFonts.say([process.argv[0], ...args].join(' '), {
+    colors: ['#f2aa4c'],
     font: 'console',
     align: 'center',
-    gradient: ['red', 'magenta']
   })
-  setupMaster({
-    exec: args[0],
-    args: args.slice(1),
+  let p = spawn(process.argv[0], args, {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
   })
-  let p = fork()
   p.on('message', data => {
     console.log('[RECEIVED]', data)
     switch (data) {
       case 'reset':
-        p.process.kill()
+        p.kill()
         isRunning = false
         start.apply(this, arguments)
         break
@@ -59,20 +45,15 @@ function start(file) {
         break
     }
   })
-  p.on('exit', (_, code) => {
+  p.on('exit', code => {
     isRunning = false
     console.error('Exited with code:', code)
     if (code === 0) return
-    watchFile(args[0], () => {
-      unwatchFile(args[0])
+    fs.watchFile(args[0], () => {
+      fs.unwatchFile(args[0])
       start(file)
     })
   })
-  let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-  if (!opts['test'])
-    if (!rl.listenerCount()) rl.on('line', line => {
-      p.emit('message', line.trim())
-    })
   // console.log(p)
 }
 
